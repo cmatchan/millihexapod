beccd99b5ff085f4d026764495303c89de2ad286
cmatchan <chrischan2008@gmail.com>
2022-06-27 19:45:00 +0000

    added test for commanding joint angle at a certain rate.

--- src/demo.py
+++ src/demo.py
@@ -7,7 +7,7 @@ from millihex_robot import Robot
 def main():
     """Initializes ROS node for robot"""
     try:
-        rospy.init_node('robot_rock', anonymous=True)       # Initialize ROS node
+        rospy.init_node('robot_walk', anonymous=True)       # Initialize ROS node
         rate = rospy.Rate(0.5)        # Set refresh rate
 
         # Initialize a Robot object to spawn millihex
@@ -15,51 +15,48 @@ def main():
         joints_per_leg = 3
         millihex = Robot(num_legs, joints_per_leg)
 
+        # Confirm that all publishers connected
         print("ROBOT INITIALIZED")
         print(f"Joint Position Publisher connections = {millihex.num_joint_publishers}\n")
 
+        # Wait for rosnode to connect to subscriber
         print("Waiting for Joint States Subscriber...")
         while millihex.subscriber.get_num_connections() < 1:
             rate.sleep()
 
         print(f"Joint States Subscriber connections = {millihex.subscriber.get_num_connections()}\n")
         
+        # Reset millihex to lying down
         millihex.lay_down()
         print(f"stance_state: {millihex.get_stance_state()}")
         print(f"swing_state: {millihex.get_swing_state()}\n")
         rate.sleep()
 
-        # for i in range((int) (num_legs / 2)):
-        #     millihex.set_joint_position(i+1, 1, -np.pi/4)
-        #     millihex.set_joint_position(i+1+3, 1, np.pi/4)
-
-        # rate.sleep()
-        # millihex.set_joint_position(1, 1, 0)
-        # rate.sleep()
-        # millihex.set_joint_position(1, 1, np.pi/4)
-        # rate.sleep()
-        # millihex.set_joint_position(1, 1, 0)
-        # rate.sleep()
-        # millihex.set_joint_position(1, 1, -np.pi/4)
-
-        # x = 0.0
-        # rate = rospy.Rate(5)
-        # while (True):
-        #     millihex.set_joint_position(1, 1, -np.sin(x * 2 / np.pi))
-        #     rate.sleep()
-        #     x = x + 0.05
-
+        # # Test millihex stand up
         # millihex.stand_up()
         # print(f"stance_state: {millihex.get_stance_state()}")
         # print(f"swing_state: {millihex.get_swing_state()}\n")
         # rate.sleep()
 
-        # Test millihex tripod gait
-        while not rospy.is_shutdown():
-            millihex.tripod_gait()
-            print(f"stance_state: {millihex.get_stance_state()}")
-            print(f"swing_state: {millihex.get_swing_state()}\n")
-            rate.sleep()
+        # Test joint movement at 100 Hz
+        x = 0.0
+        angle_rate = rospy.Rate(100)
+        while (True):
+            for i in range((int) (num_legs / 2)):
+                i = i + 1
+                angle = np.sin(x) * (3.0/4.0 * np.pi / 2)
+                print(f"angle = {angle}")
+                millihex.set_joint_position(i, 1, -angle)
+                millihex.set_joint_position(i + 3, 1, angle)
+                angle_rate.sleep()
+                x = x + 0.005
+
+        # # Test millihex tripod gait
+        # while not rospy.is_shutdown():
+        #     millihex.tripod_gait()
+        #     print(f"stance_state: {millihex.get_stance_state()}")
+        #     print(f"swing_state: {millihex.get_swing_state()}\n")
+        #     rate.sleep()
 
     except rospy.ROSInterruptException:
         pass
