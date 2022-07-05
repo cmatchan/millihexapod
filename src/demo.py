8ff6ab7d6691d74f6c51b6c2149aa85449ee6fc1
cmatchan <chrischan2008@gmail.com>
2022-07-05 19:14:26 +0000

    Cleaned up demo script.

--- src/demo.py
+++ src/demo.py
@@ -9,11 +9,10 @@ def main():
     Initializes ROS node for robot.
     """
     try:
-        rospy.init_node('robot_walk', anonymous=True)       # Initialize ROS node
-        rate = rospy.Rate(1)
-
-        # Initialize Millihexapod object
+        # Initialize ROS node and Millihexapod
+        rospy.init_node('robot_walk', anonymous=True)
         millihex = Millihexapod()
+        rate = rospy.Rate(1)
         
         # Command Millihex to lie down
         print("MILLIHEX DOWN:")
@@ -29,36 +28,6 @@ def main():
         print("MILLIHEX DOWN:")
         millihex.down()
         rate.sleep()
-
-        # # Test joint movement at 100 Hz
-        # x = 0.0
-        # angle_rate = rospy.Rate(100)
-        # while (x < np.pi):
-        #     for i in range((int) (millihex.num_legs / 2)):
-        #         i = i + 1
-        #         angle = np.sin(x) * (np.pi / 2)
-        #         print(f"angle = {angle}")
-
-        #         millihex.set_joint_position(i, 1, -angle)
-        #         millihex.set_joint_position(i + 3, 1, angle)
-
-        #         millihex.set_joint_position(i, 2, -angle)
-        #         millihex.set_joint_position(i + 3, 2, angle)
-
-        #         millihex.set_joint_position(i, 3, -angle)
-        #         millihex.set_joint_position(i + 3, 3, angle)
-
-        #         angle_rate.sleep()
-        #         x = x + 0.005
-
-        # rate.sleep()
-
-        # # Test millihex tripod gait
-        # while not rospy.is_shutdown():
-        #     millihex.tripod_gait()
-        #     print(f"stance_state: {millihex.get_stance_state()}")
-        #     print(f"swing_state: {millihex.get_swing_state()}\n")
-        #     rate.sleep()
 
     except rospy.ROSInterruptException:
         pass
