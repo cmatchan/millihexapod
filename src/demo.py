fbd0a5e49ef6b8001514cf1ad5842ca3dde38fa2
cmatchan <chrischan2008@gmail.com>
2022-07-05 17:51:40 +0000

    Cleaned up Millihexapod Class __init__ function.

--- src/demo.py
+++ src/demo.py
@@ -5,28 +5,15 @@ import numpy as np
 from millihex_robot import Millihexapod
 
 def main():
-    """Initializes ROS node for robot"""
+    """
+    Initializes ROS node for robot.
+    """
     try:
         rospy.init_node('robot_walk', anonymous=True)       # Initialize ROS node
-        rate = rospy.Rate(1)                                # Set refresh rate
+        rate = rospy.Rate(1)
 
         # Initialize Millihexapod object
         millihex = Millihexapod()
-
-        # Confirm that all publishers connected
-        print("\nMILLIHEXAPOD INITIALIZED")
-        print(f"Joint Position Publisher connections = {millihex.num_joint_publishers}\n")
-
-        print(f"Publishers:")
-        for i in range(millihex.num_joints):
-            print(f"{millihex.publishers[i].name}")
-
-        # Wait for rosnode to connect to subscriber
-        print("\nWaiting for Joint States Subscriber...")
-        while millihex.subscriber.get_num_connections() < 1:
-            rate.sleep()
-
-        print(f"Joint States Subscriber connections = {millihex.subscriber.get_num_connections()}\n")
         
         # Command Millihex to lie down
         print("Millihex lie down:")
@@ -35,46 +22,34 @@ def main():
         print(f"{millihex.joint_positions}\n")
         rate.sleep()
 
-        # Command Millihex to lie down
+        # Command Millihex to stand up
         print("Millihex stand up:")
         millihex.up()
         rate.sleep()
         print(f"{millihex.joint_positions}\n")
         rate.sleep()
 
-        # Test joint movement at 100 Hz
-        x = 0.0
-        angle_rate = rospy.Rate(100)
-        while (x < np.pi):
-            for i in range((int) (millihex.num_legs / 2)):
-                i = i + 1
-                angle = np.sin(x) * (np.pi / 2)
-                print(f"angle = {angle}")
+        # # Test joint movement at 100 Hz
+        # x = 0.0
+        # angle_rate = rospy.Rate(100)
+        # while (x < np.pi):
+        #     for i in range((int) (millihex.num_legs / 2)):
+        #         i = i + 1
+        #         angle = np.sin(x) * (np.pi / 2)
+        #         print(f"angle = {angle}")
 
-                millihex.set_joint_position(i, 1, -angle)
-                millihex.set_joint_position(i + 3, 1, angle)
+        #         millihex.set_joint_position(i, 1, -angle)
+        #         millihex.set_joint_position(i + 3, 1, angle)
 
-                millihex.set_joint_position(i, 2, -angle)
-                millihex.set_joint_position(i + 3, 2, angle)
+        #         millihex.set_joint_position(i, 2, -angle)
+        #         millihex.set_joint_position(i + 3, 2, angle)
 
-                millihex.set_joint_position(i, 3, -angle)
-                millihex.set_joint_position(i + 3, 3, angle)
+        #         millihex.set_joint_position(i, 3, -angle)
+        #         millihex.set_joint_position(i + 3, 3, angle)
 
-                angle_rate.sleep()
-                x = x + 0.005
+        #         angle_rate.sleep()
+        #         x = x + 0.005
 
-        rate.sleep()
-        
-        # # Reset millihex to lie down
-        # millihex.down()
-        # print(f"stance_state: {millihex.get_stance_state()}")
-        # print(f"swing_state: {millihex.get_swing_state()}\n")
-        # rate.sleep()
-        
-        # # Command millihex stand up
-        # millihex.up()
-        # print(f"stance_state: {millihex.get_stance_state()}")
-        # print(f"swing_state: {millihex.get_swing_state()}\n")
         # rate.sleep()
 
         # # Test millihex tripod gait
--- src/millihex_robot.py
+++ src/millihex_robot.py
@@ -12,60 +12,84 @@ NUM_JOINTS = NUM_LEGS * JOINTS_PER_LEG
 class Millihexapod:
     def __init__(self):
         """
-        Initialize ROS publishers & ROS subscribers.
+        Initialize Millihex state parameters and variables.
+        Start ROS publishers & ROS subscribers.
         """
+        print("\nInitializing Millihexapod...\n")
+        pause = rospy.Rate(1)
+        pause.sleep()
+
         # Millihex leg and joint count
         self.num_legs = NUM_LEGS
         self.joints_per_leg = JOINTS_PER_LEG
         self.num_joints = NUM_JOINTS
 
         # Array of joint positions
-        # angle range = [-pi/2, pi/2] rad
+        # angle limits = [-pi/2, pi/2] rad
         self.joint_positions = np.zeros(NUM_JOINTS)
         
         # List of joint publishers
         self.publishers = [None] * NUM_JOINTS
-        self.num_joint_publishers = 0
 
-        def start_joint_position_controller_publishers(self):
-            """
-            Initialize joint publishers to the topic:
-                /millihex/leg#_joint#_position_controller/command
-
-            Publishers named as:
-                leg#_joint#_pub
-            """
-            for i in range(NUM_LEGS):
-                for j in range(JOINTS_PER_LEG):
-                    # Index leg and joint from 1
-                    leg_number = i + 1
-                    joint_number = j + 1
-
-                    # Name of topic = "/millihex/leg#_joint#_position_controller/command"
-                    publisher_topic = f"/millihex/leg{leg_number}" \
-                        f"_joint{joint_number}_position_controller/command"
-
-                    # Initialize new publisher and add to array of publishers
-                    joint_index = (leg_number - 1) * JOINTS_PER_LEG + (joint_number - 1)
-                    start_publisher_command = f"self.publishers[{joint_index}] = " \
-                        f"rospy.Publisher('{publisher_topic}', Float64, queue_size=10, latch=True)"
-                    exec(start_publisher_command)
-
-                    # Make sure all publishers are connected before continuing
-                    check_num_publisher_connections = \
-                        f"self.publishers[{joint_index}].get_num_connections() < 1"
-                    while exec(check_num_publisher_connections):
-                        rate = rospy.Rate(100)
-                        rate.sleep()
-                    
-                    # Increment publisher count for debugging
-                    self.num_joint_publishers += 1
-
-        # Initialize all joint position controllers for command topic
-        start_joint_position_controller_publishers(self)
+        # Start all joint position controllers for /command topic
+        print("Waiting for Joint Position Controller Publishers...")
+        pause.sleep()
+        pause.sleep()
+        self.start_joint_position_controller_publishers()
+        pause.sleep()
 
         # Subscribe to /millihex/joint_states topic
+        print("\nWaiting for Joint States Subscriber...")
         self.subscriber = rospy.Subscriber("/millihex/joint_states", JointState, self.callback)
+
+        # Make sure subscriber is connected before continuing
+        while (self.subscriber.get_num_connections() < 1):
+            pause.sleep()
+        
+        print(f"{self.subscriber.name}, " \
+              f"connections = {self.subscriber.get_num_connections()}\n")
+        pause.sleep()
+
+        print("MILLIHEXAPOD INITIALIZED\n")
+        pause.sleep()
+
+
+    def get_joint_index(self, leg_number, joint_number):
+        """
+        Returns an array of updated 
+        """
+        joint_index = (leg_number - 1) * JOINTS_PER_LEG + (joint_number - 1)
+        return joint_index
+
+
+    def start_joint_position_controller_publishers(self):
+        """
+        Initialize joint publishers to the topic:
+            /millihex/leg#_joint#_position_controller/command
+        """
+        for i in range(NUM_LEGS):
+            for j in range(JOINTS_PER_LEG):
+                # Index leg and joint from 1
+                leg_number = i + 1
+                joint_number = j + 1
+
+                # Name of topic = "/millihex/leg#_joint#_position_controller/command"
+                publisher_topic = f"/millihex/leg{leg_number}" \
+                    f"_joint{joint_number}_position_controller/command"
+
+                # Initialize new publisher and add to array of publishers
+                joint_index = self.get_joint_index(leg_number, joint_number)
+                start_publisher_command = f"self.publishers[joint_index] = " \
+                    f"rospy.Publisher('{publisher_topic}', Float64, queue_size=10, latch=True)"
+                exec(start_publisher_command)
+
+                # Make sure all publishers are connected before continuing
+                while (self.publishers[joint_index].get_num_connections() < 1):
+                    rate = rospy.Rate(100)
+                    rate.sleep()
+
+                print(f"{self.publishers[joint_index].name}, " \
+                    f"connections = {self.publishers[joint_index].get_num_connections()}")
 
 
     def callback(self, ros_data):
@@ -78,7 +102,7 @@ class Millihexapod:
         rate.sleep()
 
     
-    def init_joint_position_array(self):
+    def init_rotations_array(self):
         """
         Returns a joint position array following joint axis polarity
         for right side and left side legs according to Right-Hand-Rule.
@@ -87,7 +111,7 @@ class Millihexapod:
         Positive joint effor causes left side links to rotate down.
 
         Returns:
-            [-1 -1 -1 -1 1 1 1 1]
+            [-1 -1 -1 -1 -1 -1 -1 -1 -1  1  1  1  1  1  1  1  1  1]
         """
         # Get index of middle joint
         middle_joint = int(NUM_JOINTS / 2)
@@ -99,105 +123,51 @@ class Millihexapod:
         return position_array
 
 
-    def get_joint_index(self, leg_number, joint_number):
-        """
-        Returns an array of updated 
-        """
-        joint_index = (leg_number - 1) * JOINTS_PER_LEG + (joint_number - 1)
-        return joint_index
-    
-
-    def set_joint_positions(self, new_joint_positions = np.zeros(NUM_JOINTS), move_rate = 100):
+    def set_joint_positions(self, joints = [], target_joint_position = 0, move_rate = 100):
         """
         Publishes joint_positions to corresponding joint.
 
         joint_positions is an array of length NUM_JOINTS.
         """
-        # Determine rotation direction for joints
-        rotation = self.init_joint_position_array()
-        print(f"rotation: {rotation}\n")
-        rotation[new_joint_positions < self.joint_positions] *= -1
-        print(f"rotation: {rotation}\n")
-        print(f"new_joint_positions: {new_joint_positions}\n")
-        print(f"self.joint_positions: {self.joint_positions}\n")
-        print(f"rotation: {rotation}\n")
-
+        # Set rotation speed of joint rotation
         rate = rospy.Rate(move_rate)
-        while np.any(np.absolute(self.joint_positions - new_joint_positions) > 0.1):
-            for i in range(NUM_JOINTS):
-                if ((self.joint_positions[i] - new_joint_positions[i]) > 0.1):
-                    d_theta = self.joint_positions[i] + rotation[i] * 0.01
-                    self.publishers[i].publish(d_theta)
-                
-            rate.sleep()
+
+        # Compute average of current joint angles
+        joints = np.asarray(joints)
+        current_joint_positions = self.joint_positions[joints]
+        current_joint_positions_avg = np.sum(current_joint_positions) / current_joint_positions.size
+        print(f"current_joint_positions_avg: {current_joint_positions_avg}")
+        print(f"target_joint_position: {target_joint_position}\n")
+
+        # theta = current_joint_positions_avg
+        # if (target_joint_position > current_joint_positions_avg & ):
+        #     theta 
+
+        for j in joints:
+            self.publishers[j].publish(target_joint_position)
+
+        rate.sleep()
+
 
     def up(self, joint_angle = 0.4, move_rate = 100):
         """
         Commands robot to stand up with all legs in low stance position.
         """
         # Set joint position array
-        new_joint_positions = self.init_joint_position_array()
+        new_joint_positions = self.init_rotations_array()
         new_joint_positions *= joint_angle
 
-        self.set_joint_positions(new_joint_positions, move_rate)
+        self.set_joint_positions(target_joint_positions = new_joint_positions, move_rate = move_rate)
     
 
     def down(self, move_rate = 100):
         """
         Commands robot to lay down flat.
         """
-        self.set_joint_positions(new_joint_positions = np.zeros(NUM_JOINTS), move_rate = move_rate)
+
+        joints = np.arange(NUM_JOINTS)
+        self.set_joint_positions(joints)
         
 
     def compute_ik(self):
         return
-
-
-    def tripod_gait(self):
-        """
-        Initializes and plans Tripod gait leg movement.
-        Tripod leg state pattern is the following:
-                        Leg indices = [1 2 3 4 5 6]
-        Dominant Right stance state = [1 0 1 0 1 0]
-         Dominant Left stance state = [0 1 0 1 0 1]
-        """
-        # Legs numbers for Dominant Right/Left stances
-        dominant_right_legs = np.arange(0, NUM_LEGS, 2) + 1
-        dominant_left_legs = np.arange(1, NUM_LEGS, 2) + 1
-
-        # If not in tripod gait, then make robot stand
-        # (Reset robot state to standing state)
-        if np.sum(self.stance_state) < 3:
-            self.stand_up()
-
-        # If robot in standing state, then start tripod gait
-        if np.sum(self.stance_state) == 0 and np.sum(self.swing_state) == 0:
-            # Start Tripod gait with Dominant Right stance
-            self.set_leg_stance(dominant_right_legs, stance = 1)
-            return
-
-        # If robot in Dominant Right stance, then swing Dominant Left legs
-        # Otherwise, robot in Dominant Left stance, swing Dominant Right legs
-        if np.sum(self.stance_state) == 3 and np.sum(self.swing_state) == 0:
-            if self.stance_state[0] == 1:
-                self.set_leg_swing(dominant_left_legs, swing = 1)
-            else:
-                self.set_leg_swing(dominant_right_legs, swing = 1)
-            return
-
-        if np.sum(self.stance_state) == 3 and np.sum(self.swing_state) == 3 \
-            and self.stance_state[0] != self.swing_state[0]:
-            swing_legs = np.nonzero(self.swing_state)[0] + 1
-            self.set_leg_stance(swing_legs, stance = 1)
-            return
-
-        if np.sum(self.stance_state) == 6 and np.sum(self.swing_state) == 3:
-            stance_legs = np.where(self.swing_state == 0)[0] + 1
-            self.set_leg_stance(stance_legs, stance = 0)
-            return
-
-        if np.sum(self.stance_state) == 3 and np.sum(self.swing_state) == 3 \
-            and self.stance_state[0] == self.swing_state[0]:
-            swing_legs = np.nonzero(self.swing_state)[0] + 1
-            self.set_leg_swing(swing_legs, swing = 0)
-            return
