0d666a333781efb679662ca06254256ad59c088f
cmatchan <chrischan2008@gmail.com>
2022-08-12 00:46:18 +0000

    Added back InterruptException handler in demo script.

--- src/demo.py
+++ src/demo.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
+import rospy
 import numpy as np
 from millihex_robot import Millihexapod
 
 def main():
-    millihex = Millihexapod()
+    try:
+        millihex = Millihexapod()
 
-    millihex.walk(pattern="bipod", h=(np.pi/3), w=(np.pi/3))
-    # millihex.walk(pattern="bipod", h=(np.pi/4), w=(np.pi/4))
-    # millihex.random_dancing()
+        # millihex.walk(pattern="bipod", h=(np.pi/4), w=(np.pi/4))
+        millihex.walk(pattern="bipod", h=(np.pi/3), w=(np.pi/3))
+        # millihex.random_dancing()
+
+    except rospy.ROSInterruptException:
+        pass
 
 if __name__ == '__main__':
     main()
