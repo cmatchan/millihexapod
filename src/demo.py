05bf2016b111b0cfcc74edfa5074fb6a7bb0fa50
cmatchan <chrischan2008@gmail.com>
2022-08-20 15:56:09 +0000

    Resuming data collection on local computer.

--- src/demo.py
+++ src/demo.py
@@ -22,10 +22,10 @@ def main():
     if not file_exists:
         create_results_table(database)
 
-    # Check if table is empty
-    num_rows = num_table_rows(database)[0][0]
-    if num_rows != 0:
-        clear_table(database)
+    # # Check if table is empty
+    # num_rows = num_table_rows(database)[0][0]
+    # if num_rows != 0:
+    #     clear_table(database)
 
     try:
         # Initialize Millihex robot
@@ -41,7 +41,7 @@ def main():
 
         # Start Gazebo
         millihex.spawn_model("gazebo")
-
+        
         # Parameters (N^6 data points)
         N = 4
         x_range = np.linspace(np.pi/8, np.pi/2, N)
@@ -50,18 +50,49 @@ def main():
         step_range = np.linspace(0.01, 0.5, N)
         stance_range = np.linspace(np.pi/8, np.pi/2, N)
         pattern_range = ["bipod", "tripod", "quadruped", "pentapod"]
-        
-        # Parameter sweep and data collection
+
+        param_sweep = [None]
+
+        # Construct list of parameter combinations
         for pattern in pattern_range:
             for x in x_range:
                 for z in z_range:
                     for h in h_range:
                         for step in step_range:
                             for stance in stance_range:
-                                millihex.spawn_model("obstacle", args=[f"obstacle_h:={h}"])
-                                millihex.spawn_model("millihex")
-                                (success, duration) = millihex.walk(pattern=pattern, gait_x=x, gait_z=z, stance=stance, step=step)
-                                insert_result(database, x, z, h, step, stance, pattern, success, duration)
+                                param_sweep.append([x, z, h, step, stance, pattern])
+
+        # Continue collecting data at stopped point
+        resume_point = num_table_rows(database)[0][0] + 1
+        # print(resume_point)
+        # print(f"param_sweep: {len(param_sweep[resume_point:])}")
+        for param in param_sweep[resume_point:]:
+            (x, z, h, step, stance, pattern) = param
+            millihex.spawn_model("obstacle", args=[f"obstacle_h:={h}"])
+            millihex.spawn_model("millihex")
+            (success, duration) = millihex.walk(pattern=pattern, gait_x=x, gait_z=z, stance=stance, step=step)
+            insert_result(database, x, z, h, step, stance, pattern, success, duration)
+
+        # # Parameters (N^6 data points)
+        # N = 4
+        # x_range = np.linspace(np.pi/8, np.pi/2, N)
+        # z_range = np.linspace(np.pi/8, np.pi, N)
+        # h_range = np.linspace(0.01, 0.15, N)
+        # step_range = np.linspace(0.01, 0.5, N)
+        # stance_range = np.linspace(np.pi/8, np.pi/2, N)
+        # pattern_range = ["bipod", "tripod", "quadruped", "pentapod"]
+        
+        # # Parameter sweep and data collection
+        # for pattern in pattern_range:
+        #     for x in x_range:
+        #         for z in z_range:
+        #             for h in h_range:
+        #                 for step in step_range:
+        #                     for stance in stance_range:
+        #                         millihex.spawn_model("obstacle", args=[f"obstacle_h:={h}"])
+        #                         millihex.spawn_model("millihex")
+        #                         (success, duration) = millihex.walk(pattern=pattern, gait_x=x, gait_z=z, stance=stance, step=step)
+        #                         insert_result(database, x, z, h, step, stance, pattern, success, duration)
 
         # # TESTING
         # # Gait and obstacle parameters, range=(min, max)
